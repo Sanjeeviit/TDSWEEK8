560dfdceb097241707c10aac60bca343871e52a7
SANJEEV SAH <104579917+Sanjeeviit@users.noreply.github.com>
2023-12-11 20:46:01 +0530

    Update weekapp.py

--- weekapp.py
+++ weekapp.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.title("This is Sanjeev App")
+
+def display_substrings(input_string):
+    substrings = [input_string[:i+1] for i in range(len(input_string))]
+    return substrings
+
+st.title("Display Substrings")
+input_string = "sanjeev"
+substrings = display_substrings(input_string)
+
+for substring in substrings:
+    st.write(substring)
